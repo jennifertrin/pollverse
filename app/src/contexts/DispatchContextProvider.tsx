980cecdce3b53a9619c5e593faa67d6f8ceb38bd
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-02-12 21:07:28 -0800

    add dispatch forum

---
+++ app/src/contexts/DispatchContextProvider.tsx
@@ -0,0 +1,47 @@
+import { ReactNode } from "react";
+import { DispatchProvider, DispatchAppProps } from "@usedispatch/forum";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import "@usedispatch/forum/dist/style.css";
+
+interface Props {
+    baseURL: string;
+    forumURL: string;
+    topicURL: string;
+    children?: ReactNode | ReactNode[];
+}
+
+const DispatchApp = ({ 
+    baseURL,
+    forumURL,
+    topicURL,
+    children, 
+    ...props
+}: Props) => {
+    const { connection } = useConnection();
+    const wallet = useWallet();
+
+    function buildForumPath(collectionId: string) {
+        return `${baseURL}${forumURL}/${collectionId}`;
+    }
+    
+    function buildTopicPath(collectionId: string, topicId: number) {
+        return `${baseURL}${forumURL}/${collectionId}${topicURL}/${topicId}`;
+    }
+
+    const dispatchProps : DispatchAppProps = {
+        wallet: wallet,
+        connection: connection,
+        buildForumPath: buildForumPath,
+        buildTopicPath: buildTopicPath,
+        children: children,
+        cluster: "mainnet-beta"
+    }
+
+    return (
+        <DispatchProvider {...dispatchProps}>
+            {children}
+        </DispatchProvider>
+    )
+}
+
+export default DispatchApp;
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -2,9 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ConnectionProvider  } from "@solana/wallet-adapter-react";
 import dynamic from "next/dynamic";
-
+import DispatchApp from "../contexts/DispatchContextProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
+
   const endpoint = "https://rpc.ankr.com/solana";
 
   const WalletProvider = dynamic(
@@ -17,7 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider>
+        <DispatchApp baseURL={"http://localhost:3000"} forumURL={"/forum"} topicURL={"/topic"}>
           <Component {...pageProps} />
+        </DispatchApp>
       </WalletProvider>
     </ConnectionProvider>
   );
