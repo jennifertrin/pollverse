51eab28d08d594a31d4c24a463dc24da12c559c1
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-02-20 23:40:17 -0800

    ran eslint and added key

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -11,17 +11,17 @@ export default function Home() {
   const dashboardItems = [
     {
       buttonText: "Go",
-      linkUrl: '/forum',
-      imageAlt: 'Icon of voting',
-      imageLink: '/civic-involvement.png',
+      linkUrl: "/forum",
+      imageAlt: "Icon of voting",
+      imageLink: "/civic-involvement.png",
       subTitle: "Help governments make decisions",
       title: "Your Forum",
     },
     {
       buttonText: "Go",
-      linkUrl: '/forum',
-      imageAlt: 'Icon of city view',
-      imageLink: '/city-scene.png',
+      linkUrl: "/forum",
+      imageAlt: "Icon of city view",
+      imageLink: "/city-scene.png",
       subTitle: "Experience upcoming design plans",
       title: "Your City Designs",
     },
@@ -39,14 +39,16 @@ export default function Home() {
       ) : (
         <div className="flex flex-row w-full gap-12 mt-6 mb-8 px-0 lg:px-24 mx-auto">
           {dashboardItems.map((item) => (
-            <DashboardItem
-              buttonText={item.buttonText}
-              imageAlt={item.imageAlt}
-              imageLink={item.imageLink}
-              linkUrl={item.linkUrl}
-              subTitle={item.subTitle}
-              title={item.title}
-            />
+            <div key={item.title}>
+              <DashboardItem
+                buttonText={item.buttonText}
+                imageAlt={item.imageAlt}
+                imageLink={item.imageLink}
+                linkUrl={item.linkUrl}
+                subTitle={item.subTitle}
+                title={item.title}
+              />
+            </div>
           ))}
         </div>
       )}
