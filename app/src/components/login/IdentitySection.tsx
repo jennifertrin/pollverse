cdfc10fe147544829e4f429ef975ab4b87bfb294
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-03-04 23:18:06 -0700

    chore: use gateway status to check civic pass

--- app/src/components/login/IdentitySection.tsx
+++ app/src/components/login/IdentitySection.tsx
@@ -1,20 +1,26 @@
-import React, { Dispatch, SetStateAction } from "react";
-import { IdentityButton, ButtonMode } from '@civic/solana-gateway-react';
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import { IdentityButton, ButtonMode } from "@civic/solana-gateway-react";
+import { useGateway } from "@civic/solana-gateway-react";
 
 interface Props {
   setIdentityAttempt: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function IdentitySection({setIdentityAttempt} : Props) {
+export default function IdentitySection({ setIdentityAttempt }: Props) {
+  const { gatewayStatus } = useGateway();
+
+  useEffect(() => {
+    if (gatewayStatus === 3 || gatewayStatus === 9 || gatewayStatus === 5) {
+      setIdentityAttempt(true);
+    }
+  }, [gatewayStatus]);
 
   return (
     <div className="flex flex-col font-body w-full">
       <div className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
         Verify your identity:
       </div>
-      <div onClick={() => setIdentityAttempt(true)}>
-      <IdentityButton mode={ButtonMode.LIGHT}  />
-      </div>
+      <IdentityButton mode={ButtonMode.LIGHT} />
     </div>
   );
 }
