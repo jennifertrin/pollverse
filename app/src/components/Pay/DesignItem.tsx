c79156ab0c61627c6d2c9d298860f38a5ff42766
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-03-09 10:00:54 -0800

    Add Donation Button on Designs (#22)
    * add donation button
    
    * add donation modal

--- app/src/components/Pay/DesignItem.tsx
+++ app/src/components/Pay/DesignItem.tsx
@@ -1,19 +1,29 @@
-import React from "react";
-import PhotoSphereViewer from './PhotoSphereViewer';
+import React, { useState } from "react";
+import PhotoSphereViewer from "./PhotoSphereViewer";
+import PopupModal from "./PopupModal";
 
 interface Props {
-  designTitle: string,
-  designDescription: string,
-  imageSrc: string,
+  designTitle: string;
+  designDescription: string;
+  imageSrc: string;
 }
 
-export default function DesignItem({ designDescription, designTitle, imageSrc }: Props) {
-
+export default function DesignItem({
+  designDescription,
+  designTitle,
+  imageSrc,
+}: Props) {
+  const [open, setOpen] = useState<boolean>(false);
+  
   return (
     <div className="flex flex-col gap-4 bg-slate-300 px-8 py-6 rounded-lg h-full w-full md:h-86 md:w-1/3">
       <div className="flex flex-col">
         <h1 className="flex text-4xl font-bold mt-8">{designTitle}</h1>
         <div className="flex text-md mt-4 mb-4">{designDescription}</div>
+        <button onClick={() => setOpen(!open)} className="btn btn-primary normal-case text-md text-white font-bold py-2 px-4 rounded">
+          Donate To This Project
+        </button>
+        {open ? <PopupModal amount={10} designTitle={designTitle} open={open} setOpen={setOpen} /> : null}
       </div>
       <div className="flex flex-row gap-4 m-auto mb-4 h-full w-full">
         <PhotoSphereViewer imageSrc={imageSrc} />
