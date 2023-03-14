4415d34445e0acc44940e957237877d21dbb367f
Jennifer K. Tran <38402540+jennifertrin@users.noreply.github.com>
2023-03-14 13:36:24 -0700

    Add Dialect Monitoring (#24)
    * add dialect monitoring
    
    * recreate yarn.lock
    
    * recreate yarn.lock without error
    
    * chore: declare luxon module
    
    * chore: add @stablelib/xchacha20
    
    * remove @stablelib/xchacha20
    
    * chore: recreate file to dts file

--- app/src/util/SolanaPayUtils.ts
+++ app/src/util/SolanaPayUtils.ts
@@ -6,4 +6,6 @@ export const usdcAddress = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZw
 
 export const pollverseTokenAddress = new PublicKey('4JtzydoohQxcCuasmw1tTM8WbBLt5xvVoSFwd6YZZ9W1');
 
-export const pollverseTokenAddressString = '4JtzydoohQxcCuasmw1tTM8WbBLt5xvVoSFwd6YZZ9W1'
+export const pollverseTokenAddressString = '4JtzydoohQxcCuasmw1tTM8WbBLt5xvVoSFwd6YZZ9W1';
+
+export const pollverseDAOAddress = new PublicKey('3qnpdzqPZefVvD9LjJQee8oFTQAqWTbX1f3hSeh1SYAX')
