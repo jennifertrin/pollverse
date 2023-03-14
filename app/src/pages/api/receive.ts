e37ddc4bf1fa7bca1836dae3af726ab6f45449d4
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2023-03-14 00:18:11 -0700

    deploy receive api

---
+++ app/src/pages/api/receive.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    console.log('req', req);
+    return res.status(200);
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+}
